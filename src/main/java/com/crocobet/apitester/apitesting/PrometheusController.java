9599a4553f1427b5b2149bbea90dc84d95462a42
Service Monitoring <smonitoring@crocobet.local>
2023-10-25 15:25:02 +0400

    something changed

--- src/main/java/com/crocobet/apitester/apitesting/PrometheusController.java
+++ src/main/java/com/crocobet/apitester/apitesting/PrometheusController.java
@@ -27,63 +27,63 @@ public class PrometheusController {
         this.meterRegistry = meterRegistry;
         this.apiService = apiService;
     }
-paka
-    @GetMapping("/metrics")
-    public void generatePrometheusMetrics() {
-        List<Api> apis = apiService.findAllApis();
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-
-        for (Api api : apis) {
-            List<String> urls = new ArrayList<>();
-            urls.add(api.getUrl());
-
-
-            for (String apiUrl : urls) {
-                Timer responseTimeSeconds = Timer.builder("response_time_seconds")
-                        .tag("url", api.getUrl())
-                        .register(meterRegistry);
-
-
-                Timer pingingApi = Timer.builder("pinging_api")
-                        .tag("url", api.getUrl())
-                        .register(meterRegistry);
-
-                executor.scheduleAtFixedRate(() -> {
-                    try {
-                        long startTime = System.nanoTime();
-
-                        URL url = new URI(apiUrl).toURL();
-                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                        con.setRequestMethod("GET");
-
-
-                        int responseCode = con.getResponseCode();
-
-
-                        con.disconnect();
-                        long endTime = System.nanoTime();
-
-                        double responseTimeMs = (endTime - startTime) / 1e6;
-
-                        double responseTime = (endTime - startTime) / 1e7;
-                        System.out.println("Response time for: " + api.getUrl() + " : " + responseTime);
-                        responseTimeSeconds.record((long) responseTime, TimeUnit.SECONDS);
-
-
-                        pingingApi.record(Duration.ofMillis((long) responseTimeMs));
-
-                        responseTime = 0;
-                        Gauge.builder("response_code_counter", () -> responseCode)
-                                .tag("url", api.getUrl())
-                                .register(meterRegistry);
-
-                    } catch (IOException | URISyntaxException e) {
-                        e.printStackTrace();
-                    }
-                }, 0, 15, TimeUnit.SECONDS);
-
-            }
-        }
-    }
+//paka
+//    @GetMapping("/metrics")
+//    public void generatePrometheusMetrics() {
+//        List<Api> apis = apiService.findAllApis();
+//        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+//
+//        for (Api api : apis) {
+//            List<String> urls = new ArrayList<>();
+//            urls.add(api.getUrl());
+//
+//
+//            for (String apiUrl : urls) {
+//                Timer responseTimeSeconds = Timer.builder("response_time_seconds")
+//                        .tag("url", api.getUrl())
+//                        .register(meterRegistry);
+//
+//
+//                Timer pingingApi = Timer.builder("pinging_api")
+//                        .tag("url", api.getUrl())
+//                        .register(meterRegistry);
+//
+//                executor.scheduleAtFixedRate(() -> {
+//                    try {
+//                        long startTime = System.nanoTime();
+//
+//                        URL url = new URI(apiUrl).toURL();
+//                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//                        con.setRequestMethod("GET");
+//
+//
+//                        int responseCode = con.getResponseCode();
+//
+//
+//                        con.disconnect();
+//                        long endTime = System.nanoTime();
+//
+//                        double responseTimeMs = (endTime - startTime) / 1e6;
+//
+//                        double responseTime = (endTime - startTime) / 1e7;
+//                        System.out.println("Response time for: " + api.getUrl() + " : " + responseTime);
+//                        responseTimeSeconds.record((long) responseTime, TimeUnit.SECONDS);
+//
+//
+//                        pingingApi.record(Duration.ofMillis((long) responseTimeMs));
+//
+//                        responseTime = 0;
+//                        Gauge.builder("response_code_counter", () -> responseCode)
+//                                .tag("url", api.getUrl())
+//                                .register(meterRegistry);
+//
+//                    } catch (IOException | URISyntaxException e) {
+//                        e.printStackTrace();
+//                    }
+//                }, 0, 15, TimeUnit.SECONDS);
+//
+//            }
+//        }
+//    }
 
 }
